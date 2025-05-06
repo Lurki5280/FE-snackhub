8c7edcd9ccdf80663fedf45a0dc6bc833b8d231d
21522098 <21522098@gm.uit.edu.vn>
2025-05-06 10:06:49 +0700

    change baseURl : axiosconfig

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { WHITE_LIST_ROUTES } from "../utils/appConstant";
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://backend-snack-shop.onrender.com',
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
